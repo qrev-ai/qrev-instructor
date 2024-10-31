50d388a6108ce4aa678f8657d941875ebe077e93
leeparnell <152523161+leeparnell@users.noreply.github.com>
2024-10-30 22:36:50 -0500

    Added claude-3-5-sonnet-20241022

--- qrev_instructor/models.py
+++ qrev_instructor/models.py
@@ -54,10 +54,11 @@ class APIModel(CaseInsensitiveEnum):
 
 
 class AnthropicModel(APIModel):
-    CLAUDE_3_OPUS_20240229 = "claude-3-opus-20240229"
-    CLAUDE_3_HAIKU_20240307 = "claude-3-haiku-20240307"
-    CLAUDE_3_5_SONNET_20240620 = "claude-3-5-sonnet-20240620"
     CLAUDE_3_SONNET_20240229 = "claude-3-sonnet-20240229"
+    CLAUDE_3_5_SONNET_20240620 = "claude-3-5-sonnet-20240620"
+    CLAUDE_3_5_SONNET_20241022 = "claude-3-5-sonnet-20241022"
+    CLAUDE_3_HAIKU_20240307 = "claude-3-haiku-20240307"
+    CLAUDE_3_OPUS_20240229 = "claude-3-opus-20240229"
 
 
 class OpenAIModel(APIModel):
