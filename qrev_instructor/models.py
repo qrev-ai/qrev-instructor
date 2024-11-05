dd6422355e7b4115860fee7e67d16b1f6f48925f
leeparnell <152523161+leeparnell@users.noreply.github.com>
2024-11-04 21:37:45 -0600

    Model updates

--- qrev_instructor/models.py
+++ qrev_instructor/models.py
@@ -57,7 +57,10 @@ class AnthropicModel(APIModel):
     CLAUDE_3_SONNET_20240229 = "claude-3-sonnet-20240229"
     CLAUDE_3_5_SONNET_20240620 = "claude-3-5-sonnet-20240620"
     CLAUDE_3_5_SONNET_20241022 = "claude-3-5-sonnet-20241022"
+    CLAUDE_3_5_SONNET = "claude-3-5-sonnet-latest"
     CLAUDE_3_HAIKU_20240307 = "claude-3-haiku-20240307"
+    CLAUDE_3_5_HAIKU_20241022 = "claude-3-5-haiku-20241022"
+    CLAUDE_3_5_HAIKU = "claude-3-5-haiku-latest"
     CLAUDE_3_OPUS_20240229 = "claude-3-opus-20240229"
 
 
